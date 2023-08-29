0a0cb1974bb857febae4443e3b9383b41bb9c55d
Ben Cheetham <BforBen@users.noreply.github.com>
2023-08-29 22:59:03 +0100

    Update dependencies

--- gulp/watch.js
+++ gulp/watch.js
@@ -8,11 +8,11 @@ var gulp = require('gulp')
 var config = require('./config.json')
 
 gulp.task('watch-sass', function () {
-  return gulp.watch(config.paths.assets + 'sass/**', {cwd: './'}, ['sass'])
+  return gulp.watch(config.paths.assets + 'sass/**', {cwd: './'}, gulp.series('sass'))
 })
 
 gulp.task('watch-assets', function () {
   return gulp.watch([config.paths.assets + 'images/**',
                       config.paths.assets + 'javascripts/**',
-											config.paths.assets + 'data/**'], {cwd: './'}, ['copy-assets'])
+											config.paths.assets + 'data/**'], {cwd: './'}, gulp.series('copy-assets'))
 })
